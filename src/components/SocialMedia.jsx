8eaf0613675680ee5aeb7b4e40f206009e907139
anil konda <I11200049@in.techwave.net>
2022-10-25 11:36:31 +0530

    Socialmedia links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
+// import { FaFacebookF } from 'react-icons/fa';
+// import { Link } from 'reac';
 
 const SocialMedia = () => (
   <div className='app__social'>
     <div>
-      <BsTwitter />
+      <a href='https://github.com/konda-anil' target='_blank'>
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href='https://www.linkedin.com/in/anil-konda-09b21417a/'
+        target='_blank'
+      >
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href='https://www.instagram.com/anilchathurvedi/' target='_blank'>
+        <BsInstagram />
+      </a>
     </div>
   </div>
 );
